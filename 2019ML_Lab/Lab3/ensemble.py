c3fe6ed7f5cb038e7e9abd4606c50d37980209f1
pangxiaox <517486387@qq.com>
2019-11-01 20:10:10 +0800

    add lab3

--- 2019ML_Lab/Lab3/ensemble.py
+++ 2019ML_Lab/Lab3/ensemble.py
@@ -1,4 +1,6 @@
 import pickle
+import numpy as np
+
 
 class AdaBoostClassifier:
     '''A simple AdaBoost Classifier.'''
@@ -10,21 +12,44 @@ class AdaBoostClassifier:
             weak_classifier: The class of weak classifier, which is recommend to be sklearn.tree.DecisionTreeClassifier.
             n_weakers_limit: The maximum number of weak classifier the model can use.
         '''
-        pass
+        self.weak_classifier = weak_classifier
+        self.limit = n_weakers_limit
+
+        self.weak_classifier_set = [] # the set saves all the weak classifiers
+        self.alpha_total = [] # the set saves the alpha of all weak classifiers
 
     def is_good_enough(self):
         '''Optional'''
         pass
 
-    def fit(self,X,y):
+    def fit(self, X, y):
         '''Build a boosted classifier from the training set (X, y).
 
         Args:
             X: An ndarray indicating the samples to be trained, which shape should be (n_samples,n_features).
             y: An ndarray indicating the ground-truth labels correspond to X, which shape should be (n_samples,1).
         '''
-        pass
+        self.w = np.ones(X.shape[0]) / X.shape[0]  # initialize parameter w
 
+        for i in range(self.limit):
+            classifier = self.weak_classifier.fit(X, y, self.w)
+            self.weak_classifier_set.append(classifier)
+
+            weak_predict = classifier.predict(X)
+            error = np.sum(self.w * (weak_predict != y.reshape(-1, )))
+
+            if error > 0.5:
+                break
+
+            alpha = 0.5 * np.log((1 - error)/error)
+
+            self.alpha_total.append(alpha)
+
+            z = np.multiply(self.w, np.exp(-self.alpha_total[i] * np.multiply(y.reshape(-1, ), weak_predict)))
+
+            self.w = z / np.sum(z)
+
+        return self
 
     def predict_scores(self, X):
         '''Calculate the weighted sum score of the whole base classifiers for given samples.
@@ -35,10 +60,15 @@ class AdaBoostClassifier:
         Returns:
             An one-dimension ndarray indicating the scores of differnt samples, which shape should be (n_samples,1).
         '''
-        pass
+        # initialize score
+        score = np.zeros(X.shape[0])
+        # prediction
+        for i in range(self.limit):
+            score += self.alpha_total[i]*self.weak_classifier_set[i].predict(X)
+        return score
 
     def predict(self, X, threshold=0):
-        '''Predict the catagories for geven samples.
+        '''Predict the catagories for given samples.
 
         Args:
             X: An ndarray indicating the samples to be predicted, which shape should be (n_samples,n_features).
@@ -47,12 +77,18 @@ class AdaBoostClassifier:
         Returns:
             An ndarray consists of predicted labels, which shape should be (n_samples,1).
         '''
-        pass
+        # initialize y_predict
+        y_predict = np.zeros(X.shape[0])
+        score = self.predict_scores(X)
+        # categorize into two classes
+        y_predict[np.where(score > threshold)] = 1
+        y_predict[np.where(score < threshold)] = -1
+        return y_predict
 
     @staticmethod
     def save(model, filename):
         with open(filename, "wb") as f:
-            pickle.dump(model, f)
+            return pickle.dump(model, f)
 
     @staticmethod
     def load(filename):
--- 2019ML_Lab/Lab3/train.py
+++ 2019ML_Lab/Lab3/train.py
@@ -1,6 +1,59 @@
+import os
+from PIL import Image
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+import sklearn.model_selection
+from sklearn.metrics import classification_report
+from feature import NPDFeature
+from ensemble import AdaBoostClassifier
+
+
+def get_graypic(path, x, y):
+    for file in os.listdir(path):
+        file_path = os.path.join(path, file)
+        # initialize label
+        label = None
+
+        # categorize the dataset into two parts(with label 1 or -1)
+        if path == 'datasets/original/face/':
+            label = 1
+        elif path == 'datasets/original/nonface/':
+            label = -1
+
+        # transform initial images into graypic with size 24 x 24
+        image = Image.open(file_path).convert('L').resize((24, 24))
+
+        # extract NPD features in images
+        if (x is None) & (y is None):
+            x = np.array([NPDFeature(np.asarray(image)).extract()])
+            y = np.array([label])
+        else:
+            x = np.vstack((x, NPDFeature(np.asarray(image)).extract()))
+            y = np.vstack((y, label))
+    return x, y
 
 
 if __name__ == "__main__":
-    # write your code here
-    pass
+    gray_x, gray_y = get_graypic('datasets/original/face/', None, None)
+    gray_x, gray_y = get_graypic('datasets/original/nonface/', gray_x, gray_y)
+    # print(gray_x.shape) //(1000,165600)
+    # print(gray_y.shape) //(1000,1)
+    AdaBoostClassifier.save(gray_x, 'features')
+    AdaBoostClassifier.save(gray_y, 'labels')
 
+    features = AdaBoostClassifier.load('features')
+    label = AdaBoostClassifier.load('labels')
+
+    # split the dataset with 80% for training set and 20% for testing set
+    X_train, X_test, Y_train, Y_test = sklearn.model_selection.train_test_split(features, label, test_size=0.3)
+    # train the base learners to form a boosted classifier
+    adaboostclassifier = AdaBoostClassifier(DecisionTreeClassifier(criterion="entropy",
+                                                                   max_depth=2,
+                                                                   class_weight="balanced"), 15).fit(X_train, Y_train)
+    # use boosted classifier generated above to make predictions
+    predict_y = adaboostclassifier.predict(X_test)
+
+    # save the precision, recall, f1-score information and so on to evaluate the model
+    with open('classifier_report.txt', "wb") as f:
+        report = classification_report(Y_test, predict_y, target_names=["face", "nonface"])
+        f.write(report.encode())
